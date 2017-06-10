60a56abe0d8c148b3db463726a7b2ea015245d7d
Nicole Flokos <nicole.flokos@gmail.com>
2017-06-09 22:38:13 -0400

    displaying octocats profile and followers

---
+++ app.js
@@ -0,0 +1,42 @@
+﻿var app = angular.module("app", []);
+
+
+app.controller("mainController", function ($http, $scope) {
+
+    //$http.get("followers.json")
+    //.then(function (response) {
+    //    $scope.followers = response.data;
+    //});
+
+    $http.get("https://api.github.com/users/octocat")
+        .then(function (response) {
+            $scope.user = response.data;
+        });
+
+    $http.get("https://api.github.com/users/octocat/followers")
+        .then(function (response) {
+            $scope.followers = response.data;
+        });
+
+
+
+});
+
+
+app.directive("follower", function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: "follower.html"
+    };
+
+});
+
+app.directive("user", function () {
+
+    return {
+        restrict: 'E',
+        templateUrl: "user.html"
+    };
+
+});
